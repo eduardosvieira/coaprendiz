e06d55905cb1271ce9f47db964d3475f73b876ae
eduardom <euodeionomedeusuario@gmail.com>
2018-11-19 22:19:28 -0300

    changing main routes

--- app/__init__.py
+++ app/__init__.py
@@ -2,25 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/coaprendiz/login/", methods=["GET"])
-def login():
-    return render_template("login.html")
-
+#defining route to index
 @app.route("/coaprendiz/", methods=["GET"])
 def index():
     return render_template("index.html")
 
 
+#defining route to login
+@app.route("/coaprendiz/login/", methods=["GET"])
+def login():
+    return render_template("login.html")
+
+
+#defining route to settings
 @app.route("/coaprendiz/settings/", methods=["GET"])
 def settings():
     return render_template("settings.html")
 
 
+#defining route to signup
 @app.route("/coaprendiz/signup/", methods=["GET"])
 def signup():
     return render_template("signup.html")
-
-
-@app.route("/coaprendiz/signup/2/", methods=["GET"])
-def signup2():
-    return render_template("signup2.html")
