c050f2feb8bb9edb7466ad4d0e4a6fa3ff9ef71d
Eduardo <euodeionomedeusuario@gmail.com>
2018-12-07 16:45:21 -0300

    adding new fields in Auth class

--- app/models/auth/Auth.py
+++ app/models/auth/Auth.py
@@ -3,11 +3,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app.models.auth.DatabaseFactory import DatabaseFactory
 
 class Auth():
-    def __init__(self, code=0, name="", email="", password=""):
+    def __init__(self, code=0, name="", email="", password="", skills=[], course="", nPoints=0, nVouchers=1):
         self.code = code
         self.name = name
         self.email = email
         self.password = password
+        self.skills = skills
+        self.course = course
+        self.nPoints = nPoints
+        self.nVouchers = nVouchers
         self.connection = DatabaseFactory().getConnection()
 
     def login(self, email="", password=""):
@@ -20,15 +24,11 @@ class Auth():
                 return False
         except:
             return False
-        finally:
-            print("Terminou")
 
     def signup(self, user=None):
         try:
-            user = self.connection.users.insert({
-                "email": user.email,
-                "name": user.name,
-                "password": generate_password_hash(user.password)})
+            user = self.connection.users.insert({"email": user.email, "name": user.name, "password": generate_password_hash(user.password), "skills": self.skills, "course": self.course, "nPoints": self.nPoints, "nVouchers": self.nVouchers 
+                                                })
 
             return True
         except Exception as e:
