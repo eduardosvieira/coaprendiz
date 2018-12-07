bdf3988161df23775ee97d5d2cd1ab8717f685f2
Eduardo <euodeionomedeusuario@gmail.com>
2018-12-07 16:23:24 -0300

    adding new fields in user class

--- app/models/User.py
+++ app/models/User.py
@@ -3,11 +3,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app.models.auth.DatabaseFactory import DatabaseFactory
 
 class User():
-    def __init__(self, code=0, name="", email="", password=""):
+    def __init__(self, code=0, name="", email="", password="", skills=[], course="", npoints=0, nvouchers=1):
         self.code = code
         self.name = name
         self.email = email
         self.password = password
+        self.skills = skills
+        self.course = course
+        self.npoints = npoints
+        self.nvouchers = nvouchers
         self.connection = DatabaseFactory().getConnection()
 
     def getUserByEmail(self, email=""):
@@ -15,5 +19,6 @@ class User():
             user = self.connection.users.find_one({"email": email})
 
             return user
-        finally:
-            print("Fechando conexão")
+        except Exception as e:
+            print("Houve um problema ao obter usuário por email: " + e)
+            return None
